8286473d24357a0d39c9620c0c551e1a506bf3f7
dmytro-khilchuk <dmytro@1worldonline.biz>
2018-08-26 18:40:32 +0300

    started refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'liftup';
+  squats = 100;
+  press = 105;
+  deadlift = 75;
+
+  P(percentage) {
+    return (percentage * this.press).toFixed(2) + ' = ' + percentage;
+  }
+
+  S(percentage) {
+    return (percentage * this.squats).toFixed(2) + ' = ' + percentage;
+  }
+
+  D(percentage) {
+    return (percentage * this.deadlift).toFixed(2) + ' = ' + percentage;
+  }
 }
