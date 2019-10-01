b8896829a558fb7a0a461e05b755471d620ad99d
dmytro-khilchuk <dmytro@1worldonline.biz>
2019-10-01 13:27:48 +0300

    added converter

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,6 +8,8 @@ import { RequestsUtilService } from '../../services/requests-util.service';
 })
 export class HomeComponent implements OnInit {
   news;
+  pounds;
+  kgs;
 
   constructor(
     private requestsUtil: RequestsUtilService
@@ -18,5 +20,23 @@ export class HomeComponent implements OnInit {
       .subscribe(data => {
         this.news = data;
       });
+  }
+
+  poundsChange(val) {
+    const value = Number(val);
+    if (!value) {
+      this.kgs = '';
+    } else {
+      this.kgs = (Number(value) * 0.4535).toFixed(2);
+    }
+  }
+
+  kgsChange(val) {
+    const value = Number(val);
+    if (!value) {
+      this.pounds = '';
+    } else {
+      this.pounds = (Number(value) * 2.2046).toFixed(2);
+    }
   }
 }
