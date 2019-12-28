81f95f97b85f1a6f029fcecded35e59ebc659d51
dkhilchuk <DMYTROKH@ext.inditex.com>
2019-12-28 22:26:20 +0200

    added 401 handling

--- src/app/services/requests-util.service.ts
+++ src/app/services/requests-util.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../../environments/environment';
+import { catchError, map } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +12,28 @@ import { environment } from './../../environments/environment';
 export class RequestsUtilService {
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private authService: AuthService,
+    private router: Router
   ) {
 
   }
 
   getRequest(path, params?) {
-    return this.http.get(`${environment.apiUrl}/${path}`, params);
+    return this.http.get(`${environment.apiUrl}/${path}`, params)
+      .pipe(
+        map((resp: any) => {
+          return resp;
+        }),
+        catchError((err: any) => {
+          if (err.status === 401) {
+            this.authService.logout()
+            this.router.navigate(['login']);
+          }
+
+          return of(err);
+        })
+      );
   }
 
   postRequest(path, params) {
